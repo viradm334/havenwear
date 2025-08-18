ea4408019dd0a90e32def23be3860a0d038ca5ee
viradm334 <viradamayanti@student.telkomuniversity.ac.id>
2025-08-18 11:22:21 +0700

    add frontend reset password

---
+++ src/app/reset-password/page.js
@@ -0,0 +1,8 @@
+import AuthLayout from "@/components/layout/AuthLayout";
+import ResetPasswordForm from "@/components/form/ResetPasswordForm";
+
+export default function ResetPasswordPage(){
+    return(<AuthLayout>
+        <ResetPasswordForm/>
+    </AuthLayout>)
+}
---
+++ src/components/form/ResetPasswordForm.js
@@ -0,0 +1,87 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function ResetPasswordForm() {
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    new_password: "",
+    confirm_password: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (res.ok) {
+        alert(`${data.message}`);
+        setFormData({
+          email: "",
+          password: "",
+        });
+
+        if(data.role === 'ADMIN'){
+          router.push('/admin');
+        }else{
+          router.push('/');
+        }
+      } else {
+        alert(`Error: ${data.message}`);
+      }
+    } catch (err) {
+      alert(err);
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="flex flex-col p-10 outline-gray-300 rounded bg-white outline-1 shadow-md w-1/3">
+      <h1 className="font-bold text-2xl text-center text-emerald-600 mb-5">
+        Reset Password
+      </h1>
+      <form className="flex flex-col" onSubmit={handleSubmit}>
+        <label className="text-sm text-emerald-700 font-bold mb-1.5">
+          New Password
+        </label>
+        <input
+          type="password"
+          name="new_password"
+          className="outline-1 outline-gray-400 rounded-sm mb-3 p-1.5 placeholder:text-sm placeholder:text-normal focus:outline-emerald-600"
+          placeholder="Enter your new password"
+          value={formData.new_password}
+          onChange={handleChange}
+        />
+        <label className="text-sm text-emerald-700 font-bold mb-1.5">
+          Confirm New Password
+        </label>
+        <input
+          type="password"
+          name="confirm_password"
+          className="outline-1 outline-gray-400 rounded-sm mb-6 p-1.5 placeholder:text-sm placeholder:text-normal focus:outline-emerald-600"
+          placeholder="Enter your email here"
+          value={formData.confirm_password}
+          onChange={handleChange}
+        />
+        <button className="bg-emerald-600 outline-none rounded-md w-full text-white font-bold px-1 py-2 cursor-pointer hover:bg-emerald-700 transition" type="submit">
+          Reset Password
+        </button>
+      </form>
+    </div>
+  );
+}
