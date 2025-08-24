bb3f577e6184302643824195f379b003c163cc01
viradm334 <viradamayanti@student.telkomuniversity.ac.id>
2025-08-24 17:23:31 +0700

    add different class to selected category

--- src/components/pages/UserHome.js
+++ src/components/pages/UserHome.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { formatCurrency } from "@/utils/formatCurrency";
 import Link from "next/link";
 import Carousel from "../ui/Carousel";
+import { InboxIcon } from "@heroicons/react/24/solid";
 
 export default function UserHome() {
   const [products, setProducts] = useState([]);
@@ -20,7 +21,7 @@ export default function UserHome() {
         setIsLoading(false);
       })
       .catch((err) => {
-        console.error("Failed to load products:", err);
+        console.error("Failed to load products:", err.message);
         setIsLoading(false);
       });
   }, []);
@@ -60,26 +61,31 @@ export default function UserHome() {
       <div className="outline-1 outline-gray-300 shadow-md p-5 flex flex-col gap-2 mt-8">
         <h1 className="text-xl text-gray-700 font-bold">Kategori</h1>
         <div className="flex gap-4 justify-center">
-          {categories.map((ctg, index) => (
-            <div
-              key={index}
-              className="flex flex-col p-3 outline-1 outline-gray-100 shadow-md rounded w-[120px] h-[160px] cursor-pointer"
-              onClick={() => filterProductByCategory(ctg.slug)}
-            >
-              <div className="relative w-[100px] h-[100px] overflow-hidden">
-                <Image
-                  src={ctg.imageUrl}
-                  fill
-                  alt="ctg-image"
-                  className="object-contain rounded-md"
-                />
-              </div>
+          {categories.map((ctg, index) => {
+            const isSelected = ctg.slug === selectedCategory;
+            return (
+              <div
+                key={index}
+                className={`flex flex-col p-3 outline-1 ${
+                  isSelected ? "outline-emerald-500" : "outline-gray-100"
+                } shadow-md rounded w-[120px] h-[160px] cursor-pointer`}
+                onClick={() => filterProductByCategory(ctg.slug)}
+              >
+                <div className="relative w-[100px] h-[100px] overflow-hidden">
+                  <Image
+                    src={ctg.imageUrl}
+                    fill
+                    alt="ctg-image"
+                    className="object-contain rounded-md"
+                  />
+                </div>
 
-              <h1 className="text-center text-sm text-gray-700 mb-2 font-bold">
-                {ctg.name.toUpperCase()}
-              </h1>
-            </div>
-          ))}
+                <h1 className="text-center text-sm text-gray-700 mb-2 font-bold">
+                  {ctg.name.toUpperCase()}
+                </h1>
+              </div>
+            );
+          })}
         </div>
         {selectedCategory && (
           <button
@@ -92,44 +98,51 @@ export default function UserHome() {
       </div>
       {/* End of category card */}
       <h1 className=" text-gray-700 text-xl font-bold mt-6">Rekomendasi</h1>
-      <div className="flex flex-wrap justify-start mb-6 p-10">
-        {products.map((product, index) => (
-          <Link href={`/${product.slug}`} key={product.id}>
-            {/* Image section */}
-            <div className="outline-1 outline-gray-300 rounded m-2 w-[250px] h-[250px] flex flex-col overflow-hidden">
+      <div className="flex flex-wrap mb-6 p-10">
+        {products.length > 0 ? (
+          products.map((product, index) => (
+            <Link href={`/${product.slug}`} key={product.id}>
               {/* Image section */}
-              <div className="relative w-full h-[150px]">
-                <Image
-                  src={
-                    product.productPhotos.length > 0
-                      ? product.productPhotos[0].imageUrl
-                      : "/placeholder.jpg"
-                  }
-                  fill
-                  alt="product-image"
-                  className="object-cover"
-                />
-              </div>
-
-              {/* product title and price section */}
-              <div className="flex flex-col justify-between flex-grow p-2">
-                <div className="product-title max-w-44">
-                  <h4 className="font-medium text-md text-slate-700 mb-1">
-                    {product.name}
-                  </h4>
-                  {product.totalStock <= 5 && (
-                    <p className="font-medium text-xs text-red-500">
-                      Hanya {product.totalStock} lagi!
-                    </p>
-                  )}
+              <div className="outline-1 outline-gray-300 rounded m-2 w-[250px] h-[250px] flex flex-col overflow-hidden">
+                {/* Image section */}
+                <div className="relative w-full h-[150px]">
+                  <Image
+                    src={
+                      product.productPhotos.length > 0
+                        ? product.productPhotos[0].imageUrl
+                        : "/placeholder.jpg"
+                    }
+                    fill
+                    alt="product-image"
+                    className="object-cover"
+                  />
                 </div>
-                <h5 className="text-gray-800 font-semibold text-sm">
-                  {formatCurrency(product.price)}
-                </h5>
+
+                {/* product title and price section */}
+                <div className="flex flex-col justify-between flex-grow p-2">
+                  <div className="product-title max-w-44">
+                    <h4 className="font-medium text-md text-slate-700 mb-1">
+                      {product.name}
+                    </h4>
+                    {product.totalStock <= 5 && (
+                      <p className="font-medium text-xs text-red-500">
+                        Hanya {product.totalStock} lagi!
+                      </p>
+                    )}
+                  </div>
+                  <h5 className="text-gray-800 font-semibold text-sm">
+                    {formatCurrency(product.price)}
+                  </h5>
+                </div>
               </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          ))
+        ) : (
+          <div className="flex flex-col text-gray-400 w-full justify-center items-center">
+            <InboxIcon className="size-20 mb-5" />
+            <h4 className="font-light">Produk tidak ditemukan</h4>
+          </div>
+        )}
       </div>
     </div>
   );
