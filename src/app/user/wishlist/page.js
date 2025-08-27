76134773337954b92435ba38923cf86df7426f6f
viradm334 <viradamayanti@student.telkomuniversity.ac.id>
2025-08-28 00:26:24 +0700

    add suspense to wishlist

--- src/app/user/wishlist/page.js
+++ src/app/user/wishlist/page.js
@@ -1,10 +1,15 @@
-'use client'
+"use client";
 
-import UserLayout from "@/components/layout/UserLayout"
-import UserWishlist from "@/components/pages/UserWishlist"
+import UserLayout from "@/components/layout/UserLayout";
+import UserWishlist from "@/components/pages/UserWishlist";
+import { Suspense } from "react";
 
-export default function UserWishlistPage(){
-    return(<UserLayout title="Wishlist">
-        <UserWishlist/>
-    </UserLayout>)
+export default function UserWishlistPage() {
+  return (
+    <UserLayout title="Wishlist">
+      <Suspense>
+        <UserWishlist />
+      </Suspense>
+    </UserLayout>
+  );
 }
